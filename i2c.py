dfe7a56ffefba056e7e7d9b84d01fc2aba7c5046
Daniel Trnka <daniel.trnka@gmail.com>
2017-11-26 12:09:14 +0000

    i2c led blinking test

---
+++ i2c.py
@@ -0,0 +1,23 @@
+import smbus
+import time
+
+BUS = 1
+ADDR = 0x8
+
+LEDS = [4, 1]
+
+bus = smbus.SMBus(BUS)
+
+
+def digitalWrite(pin, state):
+    try:
+        bus.write_byte_data(ADDR, pin, state)
+    except IOError as e:
+        print(e)
+
+
+while True:
+    for led in LEDS:
+        digitalWrite(led, 1)
+        time.sleep(1)
+        digitalWrite(led, 0)
