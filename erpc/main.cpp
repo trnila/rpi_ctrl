304025c686bde49c0e289f453e427c34a92a6f07
Daniel Trnka <daniel.trnka@gmail.com>
2017-11-30 18:46:53 +0100

    erpc: blink at 921600

--- erpc/main.cpp
+++ erpc/main.cpp
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <array>
+#include <termios.h>
 #include "erpc_client_setup.h"
 #include "messages.h"
 
 const auto PORT = GPIO_B;
-const std::array<int, 4> LEDS{0, 3, 4, 5};
+const std::array<int, 3> LEDS{0, 3, 4};
+const char *SERIAL_PATH = "/dev/ttyAMA0";
+const int BAUD_RATE = B921600;
 
 int main() {
-	erpc_transport_t transport = erpc_transport_serial_init("/dev/ttyAMA0", 9600);
+	erpc_transport_t transport = erpc_transport_serial_init(SERIAL_PATH, BAUD_RATE);
 	erpc_mbf_t message_buffer_factory = erpc_mbf_dynamic_init();
 	erpc_client_init(transport, message_buffer_factory);
 
@@ -18,7 +21,7 @@ int main() {
 			printf("set_led(PORTB, %d, %d) = ", 1 << led, state);
 			printf("%d\n", set_led(PORT, 1 << led, state));
 
-			sleep(1);
+			usleep(100 * 1000);
 		}
 		state = !state;
 	}
